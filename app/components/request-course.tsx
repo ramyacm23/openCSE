5d82548dfabad17fcaf1663b21ccd03ffec86c8b
Safwan Shaik <safwanshk11@gmail.com>
2026-05-17 10:39:50 +0530

    request course changes

--- app/components/request-course.tsx
+++ app/components/request-course.tsx
@@ -13,7 +13,7 @@ export default function RequestCourse() {
         href="https://github.com/pushkarscripts/opencse/issues/new?template=course_request.md"
         target="_blank"
         rel="noopener noreferrer"
-        className="inline-block mt-6 px-6 py-3 rounded-lg bg-white text-black font-semibold hover:opacity-80 transition"
+        className="inline-block mt-6 px-6 py-3 rounded-lg bg-[#d2b48c] text-black font-semibold hover:opacity-80 transition"
       >
         Request a Course
       </a>
